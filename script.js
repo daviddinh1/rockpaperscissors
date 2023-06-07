2bf9070d5f261d55f8d724a9184fd0cd4bd61bba
daviddinh1 <91696365+daviddinh1@users.noreply.github.com>
2023-06-06 21:13:24 -0500

    Added playRound function that keeps track of score and has the winning conditions and added game function that allows the game to run up to 5 rounds displaying the scores and who won

--- script.js
+++ script.js
@@ -4,4 +4,54 @@ function getComputerChoice() {
  return choice;
 }
 
-console.log(getComputerChoice());
+let userScore = 0;
+let computerScore =0;
+
+function playRound(playerSelection, computerSelection){
+ let userInput = playerSelection.toLowerCase();
+
+ if(userInput === computerSelection){
+  userScore++;
+  computerScore++;
+  return "Tie!! BOZO";
+ } else if(userInput === 'rock' && computerSelection ==='scissors'){
+  userScore++;
+  return "You picked rock! Rock beats scissors you WON!";
+ } else if(userInput === 'paper' && computerSelection ==='rock'){
+   userScore++;
+  return "You picked paper! paper beats rock you WON!";
+ } else if(userInput === 'scissors' && computerSelection ==='paper'){
+   userScore++;
+  return "You picked scissors! scissors beats paper you WON!";
+ }else if(computerSelection === 'rock' && userInput ==='scissors'){
+  computerScore++;
+  return "Bot picked rock! Rock beats scissors you LOST!";
+ } else if(computerSelection === 'paper' && userInput ==='rock'){
+  computerScore++;
+  return "Bot picked paper! paper beats rock you LOST!";
+ } else if(computerSelection === 'scissors' && userInput ==='paper'){
+  computerScore++;
+  return "BOT picked scissors! scissors beats paper you LOST!";
+ }
+
+}
+
+function game(){
+ for(let i = 0 ; i<5 ;i++){
+  let playerSelection = prompt("Please enter rock, paper, or scissors");
+  let computerSelection = getComputerChoice();
+  console.log(playRound(playerSelection,computerSelection));
+  let display = `SCORES: User: ${userScore} Bot: ${computerScore}`;
+  console.log(display);
+ }
+ if(userScore > computerScore){
+  console.log("you won"); 
+ }
+ else{
+  console.log("you lost");
+ }
+}
+
+game();
+
+
